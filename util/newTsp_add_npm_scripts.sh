a20fd68945948e8faabd8ac3e48acc00b88e2500
Imre Toth <tothimre@gmail.com>
2025-01-17 01:48:33 +0100

    Add NPM Scripts for TypeScript Starter Package
    Add necessary npm scripts for the TypeScript starter package,
    including scripts for starting the application, running tests,
    building the project, and a development mode. These scripts will
    streamline the development workflow and ensure consistent build processes.
    
    Additionally, includes "dest/src*" in the "files" field of package.json.
    
    uuid-stamp: 3062d48a-d943-4aa3-b47b-13bf3d218d8d

--- util/newTsp_add_npm_scripts.sh
+++ util/newTsp_add_npm_scripts.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 newTsp_add_npm_scripts() {
-  jq '.scripts = { 
-    "start": "npx ts-node src/index.ts", 
-    "test": "npx jest --coverage", 
+  jq '.scripts = {
+    "start": "npx ts-node src/index.ts",
+    "test": "npx jest --coverage",
     "test:ci": "npx jest --ci --coverage",
-    "build": "npx tsc", 
+    "build": "npx tsc",
     "start:build": "npm run build && node dest/index.js",
     "dev": "npx ts-node-dev --respawn --transpileOnly src/index.ts"
-    } 
+  },
+  "files": ["dest/src*"]
   ' package.json > temp.json && mv temp.json package.json
 }
