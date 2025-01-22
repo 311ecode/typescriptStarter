885fb8ce16eda2960b627e43661c6acccce32277
Imre Toth <tothimre@gmail.com>
2025-01-22 14:22:16 +0100

    Refactor TypeScript Starter Project Structure
    Refactors the TypeScript starter project for improved structure and clarity.
    
    This commit removes unnecessary scripts from `newTsp.sh` and consolidates
    script logic into `newTsp_init_npm.sh`.  Additionally, the commit
    updates build and testing scripts to utilize tsup for enhanced module
    bundling and transpilation.
    
    The removal of extraneous functionality streamlines the project setup process and improves maintainability.
    
    uuid-stamp: 56bf7c1b-1ce9-4ad1-bd1d-6ba59c529b59

--- newTsp.sh
+++ newTsp.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 newTsp() {
   newTsp_create_project_dir "$1"
-  newTsp_init_git
   newTsp_init_npm
   newTsp_install_deps
   newTsp_init_typescript
   newTsp_create_index_ts
   newTsp_create_gitignore
-  newTsp_add_npm_scripts
-
+  
   echo "
   Project initialized successfully!
   "
--- util/newTsp_init_npm.sh
+++ util/newTsp_init_npm.sh
@@ -8,7 +8,24 @@ newTsp_init_npm() {
     "types": "dest/src/index.d.ts",
     "type": "module",
     "files": [
-      "dest/src*/**"
+      "dist/src/**"
     ]
   }' package.json > temp.json && mv temp.json package.json
+
+  jq '.scripts = {
+    "tsart": "npx ts-node src/index.ts",
+    "start": "node dist/index.js",
+    "test": "npx jest --coverage",
+    "test:ci": "npx jest --ci --coverage",
+    "build": "tsup src/index.ts --format esm,cjs --dts --clean",
+    "dev": "tsup src/index.ts --format esm,cjs --watch",
+    "start:build": "npm run build && node dest/index.js",
+    "dev": "npx ts-node-dev --respawn --transpileOnly src/index.ts",
+    "lint": "ts-standard",
+    "lint:fix": "ts-standard --fix",
+    "format": "prettier --write src/**/*.ts",
+    "typecheck": "tsc",
+    "watch": "tsc --watch" 
+  }' package.json > temp.json && mv temp.json package.json
+
 }
